07e8e741b37f82f630935d70ec30b9b47758b1ff
saraGlez1980 <sara.glez.cuervo@gmail.com>
2025-03-03 13:28:07 +0100

    añadidos componentes a la vista

--- src/sooper/demo/tkrun/SupermercadoView.java
+++ src/sooper/demo/tkrun/SupermercadoView.java
@@ -16,11 +16,21 @@ import java.awt.event.ActionEvent;
 import sooper.demo.tkrun.*;
 import java.awt.BorderLayout;
 import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
 
 public class SupermercadoView {
 
 	
 	protected JFrame frmSooper;
+	private JTextField textField;
+	private JTable table;
+	private JTable table_1;
 	
 	public SupermercadoView() {
 		initialize();
@@ -32,7 +42,47 @@ public class SupermercadoView {
 		frmSooper = new JFrame();
 		frmSooper.setTitle("SOOPER");
 		frmSooper.setBounds(0, 0, 496, 496);
-		frmSooper.getContentPane().setLayout(new MigLayout("", "[grow][grow][grow][grow]", "[][][grow][][][][][][][][]"));
+		frmSooper.getContentPane().setLayout(new MigLayout("", "[]", "[][][][][grow][]"));
+		
+		textField = new JTextField();
+		textField.setText("<introduzca aqui el numero de pedido>");
+		frmSooper.getContentPane().add(textField, "cell 0 0,growx");
+		textField.setColumns(10);
+		
+		JButton btnAniadirPedido = new JButton("Añadir Pedido");
+		frmSooper.getContentPane().add(btnAniadirPedido, "cell 0 1");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		frmSooper.getContentPane().add(scrollPane, "cell 0 2,grow");
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"idArticulo", "Descripcion"
+			}
+		));
+		scrollPane.setViewportView(table);
+		
+		JButton btnEmbolsarArticulos = new JButton("Embolsar");
+		frmSooper.getContentPane().add(btnEmbolsarArticulos, "cell 0 3,aligny bottom");
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		frmSooper.getContentPane().add(scrollPane_1, "cell 0 4,grow");
+		
+		table_1 = new JTable();
+		table_1.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"idArticulo", "idContenedor", "Tipo Contenedor"
+			}
+		));
+		scrollPane_1.setViewportView(table_1);
+		
+		JButton btnEnviarAlmacen = new JButton("Enviar a almacen");
+		frmSooper.getContentPane().add(btnEnviarAlmacen, "cell 0 5");
 		frmSooper.setVisible(true);
 		
 	}
